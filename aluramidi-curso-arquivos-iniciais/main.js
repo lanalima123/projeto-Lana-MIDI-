f621b591bc96241230b1308f73d6f71b3a814352
lanalima123 <alcylima03@gmail.com>
2024-04-13 20:31:32 -0300

    melhorado o codigo

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,16 +1,37 @@
-const listaDeTeclas = document.querySelectorAll('.tecla')
-let contador = 0
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+let tecla = listaDeTeclas.length;
+
 function somTecla(idElementoAudio){
-    document.querySelector(idElementoAudio).play()
-}
-let tecla = listaDeTeclas.length
-while(contador < tecla){
+    let seletor = document.querySelector(idElementoAudio)
+    if(seletor != null  && seletor.localName === 'audio'){seletor.play()}else{
+        console.log('desculpe! evento nao encontrado')
+    }
+   
+    
+};
+
+/*while(contador < tecla){
     let parteDoId = listaDeTeclas[contador].classList[1];
-    let aoClicarNoBotao = listaDeTeclas[contador]
-    aoClicarNoBotao.onclick = function () {somTecla(`#som_${parteDoId}`)
+    let aoClicarNoBotao = listaDeTeclas[contador];
+    aoClicarNoBotao.onclick = function () {somTecla(`#som_${parteDoId}`);
 
     };
-    contador++
-    console.log(parteDoId)
-    console.log(aoClicarNoBotao)
+    contador++;
+    console.log(parteDoId);
+    console.log(aoClicarNoBotao);
+};*/
+
+for(let contador = 0; contador < tecla; contador++){
+    let parteDoId = listaDeTeclas[contador].classList[1];
+    let aoClicarNoBotao = listaDeTeclas[contador];
+    aoClicarNoBotao.onclick = function () {somTecla(`#som_${parteDoId}`)};
+    aoClicarNoBotao.onkeydown = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+        aoClicarNoBotao.classList.add('ativa')
+    }
 }
+    aoClicarNoBotao.onkeyup = function () {aoClicarNoBotao.classList.remove('ativa')}
+    
+   console.log(contador);
+};
